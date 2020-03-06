cd22aa4d19f7cb2274cdb0066ba1f55d17e27217
pRoy24 <roypritam1234@gmail.com>
2020-03-06 14:56:33 +0000

    remove extra whitespace

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,21 +13,20 @@ router.get('/', function(req, res, next) {
 		  
 		  page.content().then(function(pageData){
 		  		 page.$$('.listing-grid__item').then(function(items){
-		  				
-		  				let priceList = items.map(function(item){
-		  					return item.$eval('.listing-grid__item--name', elem => elem.innerText).then(function(symbol){
-								let symbolParsed = symbol.split("/");
-							let quote = symbolParsed[0].trim();
-							let base = symbolParsed[1].trim();
-			
-							if (base === 'USD') {
-								return item.$eval('.listing-grid__item--answer', elem => elem.innerText).then(function(price){
-									let priceParsed = price.split("$")[1].trim();
-									return {symbol: quote, price: priceParsed};
-									});
-							} else {
-									return null;
-							}  						
+	  				let priceList = items.map(function(item){
+	  					return item.$eval('.listing-grid__item--name', elem => elem.innerText).then(function(symbol){
+						let symbolParsed = symbol.split("/");
+						let quote = symbolParsed[0].trim();
+						let base = symbolParsed[1].trim();
+		
+						if (base === 'USD') {
+							return item.$eval('.listing-grid__item--answer', elem => elem.innerText).then(function(price){
+								let priceParsed = price.split("$")[1].trim();
+								return {symbol: quote, price: priceParsed};
+							});
+						} else {
+							return null;
+						}  						
 				  	});
 		  		});
 		  		
